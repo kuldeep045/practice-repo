fe36460f91f04031d9ff7cd43795ad2582193a95
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-05-22 17:59:02 +0545

    data types

---
+++ 01_basis/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a newer version
+// alert(1+4); // shows pop ups in browser
+
+console.log(3
+    +
+    3) // code readibility is bad
+
+
+// primitive datatypes
+// number => 2 power 53
+// bigint => large numbers
+// string = ""
+// null = standalone value 
+// undefined => value is not assigned
+// symbol => unique
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+console.log(typeof 9) //number
+console.log(typeof true) //boolean
+console.log(typeof "Kuldeep") //string
