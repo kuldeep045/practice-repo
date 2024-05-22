18bd59b91248d4d917f40a3f2bf5a1213e9a6ccb
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-05-22 18:00:47 +0545

    operations

---
+++ 01_basis/operations.js
@@ -0,0 +1,29 @@
+// var score = "44"
+// console.log(typeof score) //string
+
+// var value = Number(score) // N should be capital
+// // also can be converted to String and Boolean 
+// console.log(typeof value) //number
+
+// // "44" => 44
+// // "44abc" => NaN
+// //true => 1 & false => 0
+// // if u convert null to number than it will give 0
+// let isLoggedIn = 0;
+// let boolean = Boolean(isLoggedIn)
+// console.log(boolean) // false
+
+
+// *********************Operations******************//
+let value = 2
+let newValue = -value
+// console.log(newValue)
+
+// console.log(2+2) //add
+// console.log(2-2) //subtract
+// console.log(2*2) //multiply
+// console.log(2**2) //power
+// console.log(3/2) //divide
+// console.log(3%2) //shows remainder
+
+
