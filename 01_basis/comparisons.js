743949329d2092eb790d23fb16b1d502e5462a77
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-05-22 18:01:39 +0545

    comparisons

---
+++ 01_basis/comparisons.js
@@ -0,0 +1,13 @@
+
+
+// console.log(null==0)   //false
+// console.log(null===0)  //false
+// console.log(null!=0)   //true
+// console.log(null<=0)   //true
+// console.log(null>=0)   //true
+
+// console.log(undefined==0)   //false
+// console.log(undefined===0)  //false
+// console.log(undefined!=0)   //true
+// console.log(undefined<=0)   //false
+// console.log(undefined>=0)   //false
