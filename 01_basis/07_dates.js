03f9a759a7adbeea33a941f176d9a72fa3537eef
Kuldeep Kumar Mahato <cooldeep788@gmail.com>
2024-05-23 06:21:23 +0000

    date

---
+++ 01_basis/07_dates.js
@@ -0,0 +1,8 @@
+const myDate = new Date();
+// console.log(myDate.toString())
+// console.log(typeof myDate) //object
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+
+const myCreatedDate = new Date(2024, 0, 29)
+// console.log(myCreatedDate.toDateString())
